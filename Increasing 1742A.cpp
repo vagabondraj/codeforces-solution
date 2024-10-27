e7ba4855b801ca9a013785d51b51e57cbfb0837c
vagabondraj <raviraj06112005@gmail.com>
2024-10-27 14:51:35 +0530

    New solution added

---
+++ Increasing 1742A.cpp
@@ -0,0 +1,31 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    int n;
+
+    while(t--)
+    {
+        cin >> n;
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a,a+n);
+        for(int i=0; i<n; i++)
+        {
+            if(a[i] == a[i+1])
+            {
+                cout << "NO" << endl;
+                break;
+            } else{
+                cout << "YES" << endl;
+                break;
+            }
+        }
+    }
+}
