c2d6e11063d78e64add0f283003685ae9e35d4cf
vagabondraj <raviraj06112005@gmail.com>
2024-12-13 12:43:14 +0530

    Polycarp and Coins

--- 1551A.cpp
+++ 1551A.cpp
@@ -33,3 +33,37 @@ int main()
     }
     return 0;
 }
+
+
+<< ---- java code ---->>
+
+import java.util.*;
+
+public class Main
+{
+   public static void work(int n)
+   {
+    int cnt1 = n/3;
+    int cnt2 = cnt1;
+    if(n%3 == 1)
+    {
+        cnt1+=1;
+    }else if(n%3 == 2)
+    {
+        cnt2+=1;
+    }
+    System.out.println(cnt1 + " " + cnt2);
+   }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0) 
+        {
+            int n= sc.nextInt();
+            work(n);
+        }
+        sc.close();
+    }
+}
